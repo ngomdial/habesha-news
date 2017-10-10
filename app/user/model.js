c713b6f8cbefa44179979fe960ffdcfb88bc00a2
saladthieves <muhiregide@gmail.com>
2017-10-10 15:48:26 +0300

    Add user schema

---
+++ app/user/model.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+        username:   {type: String, required: true},
+        email:      {type: String, required: true},
+        password:   {type: String, required: true},
+        profile:    {type: Schema.Types.ObjectId, ref: 'Profile'}
+    },
+    {
+        timestamps: {createdAt: 'created_at', updatedAt: 'updated_at'}
+    }
+);
+
+module.exports = mongoose.model('User', userSchema);

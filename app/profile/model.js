b83117a387a8b775585452b7835903f6f85589ea
saladthieves <muhiregide@gmail.com>
2017-10-10 16:09:11 +0300

    Add profile model

---
+++ app/profile/model.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const profileSchema = new Schema({
+        picture_url:            {type: String},
+        enable_notifications:   {type: Boolean, default: true},
+        active:                 {type: Boolean, default: true},
+        user:                   {type: Schema.Types.ObjectId, ref: 'User', required: true},
+        categories:             [{type: Schema.Types.ObjectId, ref: 'Category'}]
+    },
+    {
+        timestamps: {createdAt: 'created_at', updatedAt: 'updated_at'}
+    }
+);
+
+module.exports = mongoose.model('Profile', profileSchema);

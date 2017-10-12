976b155dcc517a48dbe24b65f902ab57fe918b9f
saladthieves <muhiregide@gmail.com>
2017-10-12 19:12:20 +0300

    Update profile dal.js tests

--- test/app/profile/dal-test.js
+++ test/app/profile/dal-test.js
@@ -14,6 +14,9 @@ const dal = require('../../../app/profile/dal');
 
 describe('profile dal.js', () => {
 
+    const user_id = '59de86e5d84d75001f1fc6b0';
+    const user_id_other = '59de86e5d84d75001f1fc6b1';
+
     beforeEach(() => {
         Profile.remove({}).exec();
     });
@@ -23,11 +26,58 @@ describe('profile dal.js', () => {
     });
 
     it('Calls create(user_id)', () => {
-        const user_id = '59de86e5d84d75001f1fc6b0';
         return dal.create(user_id)
             .then(profile => {
                 expect(profile).to.be.a('object');
                 sinon.assert.match(profile.user.toString(), user_id);
+            });
+    });
+
+    it('Calls findOne(query)', () => {
+        return dal
+            .create(user_id)
+            .then(() => {
+                return dal.findOne({user: user_id});
+            })
+            .then(profile => {
+                expect(profile).to.be.a('object');
+                sinon.assert.match(profile.user.toString(), user_id);
+            });
+    });
+
+    it('Calls findOne(query) with non-existing user_id', () => {
+        return dal
+            .create(user_id)
+            .then(() => {
+                return dal.findOne({user: user_id_other});
+            })
+            .then(profile => {
+                expect(profile).to.be.a('null');
+            });
+    });
+
+    it('Calls findAll()', () => {
+        return dal
+            .findAll()
+            .then(profiles => {
+                expect(profiles).to.be.a('array');
+                expect(profiles).to.be.empty;
+            });
+    });
+
+    it('Calls findAll() with some data', () => {
+        return dal
+            .create(user_id)
+            .then(() => {
+                return dal.create(user_id_other);
+            })
+            .then(() => {
+                return dal.findAll();
+            })
+            .then(profiles => {
+                expect(profiles).to.be.a('array');
+                expect(profiles).to.not.be.empty;
+                expect(profiles).to.have.lengthOf(2);
             });
     });
 

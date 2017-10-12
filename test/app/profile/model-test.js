d362c3791919a2c38bc8d8e67205655ed33b825a
saladthieves <muhiregide@gmail.com>
2017-10-12 19:47:59 +0300

    Add profile model test

---
+++ test/app/profile/model-test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+const sandbox = sinon.createSandbox();
+
+const Profile = require('../../../app/profile/model');
+
+describe('profile mode.js', () => {
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    it('Should fail upon saving, if user does not exist', done => {
+        let profile = new Profile();
+
+        profile.validate(err => {
+            expect(err.errors.user).to.exist;
+            done();
+        });
+    });
+});
+
+// TODO: Write tests for adding categories

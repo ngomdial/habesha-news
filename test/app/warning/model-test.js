7b3e837264e607bc6d43ac19e255e903f3ea9577
saladthieves <muhiregide@gmail.com>
2017-10-13 15:12:40 +0300

    Add warning model test

---
+++ test/app/warning/model-test.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+const Promise = require('bluebird');
+
+const Warning = require('../../../app/warning/model');
+
+describe('warning model.js', () => {
+    let sandbox;
+
+    beforeEach(() => {
+        sandbox = sinon.createSandbox();
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    it('Should fail upon saving, if message is missing', () => {
+        let warning = new Warning();
+
+        return Promise.resolve(() => {
+            warning.validate(err => {
+                expect(err.errors.message).to.exist;
+            });
+        });
+    });
+
+    it('Should fail upon saving, if poster is missing', () => {
+        let warning = new Warning();
+
+        return Promise.resolve(() => {
+            warning.validate(err => {
+                expect(err.errors.poster).to.exist;
+            });
+        });
+    });
+
+    it('Should fail upon saving, if article is missing', () => {
+        let warning = new Warning();
+
+        return Promise.resolve(() => {
+            warning.validate(err => {
+                expect(err.errors.article).to.exist;
+            });
+        });
+    });
+});

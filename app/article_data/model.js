955cdb704315d0681875365c9d56f7399f2f81ce
saladthieves <muhiregide@gmail.com>
2017-10-13 13:31:45 +0300

    Add article data model

---
+++ app/article_data/model.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const articleDataSchema = new Schema({
+        article:    {type: Schema.Types.ObjectId, ref: 'Article', required: true},
+        comments:   [{type: Schema.Types.ObjectId, ref: 'Comment'}],
+        followers:  [{type: Schema.Types.ObjectId, ref: 'User'}],
+        warnings:   [{type: Schema.Types.ObjectId, ref: 'Warning'}],
+        voters:     [{type: Schema.Types.ObjectId, ref: 'User'}],
+    },
+    {
+        timestamps: {createdAt: 'created_at', updatedAt: 'updated_at'}
+    }
+);
+
+module.exports = mongoose.model('ArticleData', articleDataSchema);
--- app/category/model.js
+++ app/category/model.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const categorySchema = new Schema({
-        name: {type: String, required: true},
-        color: {type: String}
+        name:   {type: String, required: true},
+        color:  {type: String}
     },
     {
         timestamps: {createdAt: 'created_at', updatedAt: 'updated_at'}

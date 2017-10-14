d1522ed4c873dcdfc327874245c2f16e96129073
saladthieves <muhiregide@gmail.com>
2017-10-14 17:54:00 +0300

    Fix model validation test

--- test/app/comment/model-test.js
+++ test/app/comment/model-test.js
@@ -3,7 +3,6 @@
 const chai = require('chai');
 const expect = chai.expect;
 const sinon = require('sinon');
-const Promise = require('bluebird');
 
 const Comment = require('../../../app/comment/model');
 
@@ -14,23 +13,13 @@ describe('comment model.js', () => {
         sandbox = sinon.createSandbox();
     });
 
-    it('Should fail upon saving, if message is missing', () => {
+    it('Should fail upon saving, if model is empty', done => {
         let comment = new Comment();
 
-        return Promise.resolve(() => {
-            comment.validate(err => {
-                expect(err.errors.message).to.exist;
-            });
-        });
-    });
-
-    it('Should fail upon saving, if poster is missing', () => {
-        let comment = new Comment();
-
-        return Promise.resolve(() => {
-            comment.validate(err => {
-                expect(err.errors.poster).to.exist;
-            });
+        comment.validate(err => {
+            expect(err.errors.message).to.exist;
+            expect(err.errors.poster).to.exist;
+            done();
         });
     });
 

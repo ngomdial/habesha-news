6e2952cad926b8abd58f0349f231fea14c1d5dbb
saladthieves <muhiregide@gmail.com>
2017-10-14 17:44:00 +0300

    Fix article model validation tests

--- test/app/article/model-test.js
+++ test/app/article/model-test.js
@@ -3,7 +3,6 @@
 const chai = require('chai');
 const expect = chai.expect;
 const sinon = require('sinon');
-const Promise = require('bluebird');
 
 const Article = require('../../../app/article/model');
 
@@ -14,18 +13,16 @@ describe('article model.js', () => {
         sandbox = sinon.createSandbox();
     });
 
-    it('Should fail upon saving, if headline, source_url, image_url, summary, category, poster are missing', () => {
+    it('Should fail upon saving, if headline, source_url, image_url, summary, category, poster are missing', done => {
         let article = new Article();
-
-        return Promise.resolve(() => {
-            article.validate(err => {
-                expect(err.errors.headline).to.exist;
-                expect(err.errors.source_url).to.exist;
-                expect(err.errors.image_url).to.exist;
-                expect(err.errors.summary).to.exist;
-                expect(err.errors.category).to.exist;
-                expect(err.errors.poster).to.exist;
-            });
+        article.validate(err => {
+            expect(err.errors.headline).to.exist;
+            expect(err.errors.source_url).to.exist;
+            expect(err.errors.image_url).to.exist;
+            expect(err.errors.summary).to.exist;
+            expect(err.errors.category).to.exist;
+            expect(err.errors.poster).to.exist;
+            done();
         });
     });
 

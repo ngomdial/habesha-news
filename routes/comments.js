d9d5cbb486b1d0b59dc99213762ea12361d2cebf
saladthieves <muhiregide@gmail.com>
2017-10-15 14:53:13 +0300

    Add posting of comment on article data

--- routes/comments.js
+++ routes/comments.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 const controller = require('../app/comment/cont');
 
 router.get('/', controller.findAll);
+router.post('/', controller.create);
 
 router.param('comment_id', controller.validateOne);
 router.get('/:comment_id', controller.findOne);
